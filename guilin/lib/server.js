153594120fb22d4fd36f94c808c842766dfe719e
337547038 <337547038@qq.com>
2018-12-11 11:43:23 +0800

    运行server时添加端口占用判断

--- guilin/lib/server.js
+++ guilin/lib/server.js
@@ -1,22 +1,27 @@
 ﻿var fs = require("fs");
 var exec = require('child_process').exec;
 var watch = require('./watch');
+var net = require('net');
 module.exports = function () {
   var package = JSON.parse(fs.readFileSync('./package.json'));
   var build = "";
-  if (package.dist != "") {
+  if (package.dist !== "") {
     build = '--content-base ' + package.dist + '/';
   }
+
   var serverIp = "";
-  if (package.serverIp != "") {
+  if (package.serverIp !== "") {
     serverIp = ' --host ' + package.serverIp;
   }
-  var url = 'http://localhost:' + package.port + '/';
-  if (package.serverIp != "") {
-    url = 'http://' + package.serverIp + ':' + package.port + '/';
+  var url = 'http://localhost:';
+  let portUrl = 'localhost';
+  if (package.serverIp !== "") {
+    url = 'http://' + package.serverIp + ':';
+    portUrl = package.serverIp
   }
+  const port = portIsOccupied(package.port, portUrl);
   //var option = 'webpack-dev-server --progress --colors --hot --inline ' + build + serverIp + ' --port ' + package.port + ' ';
-  var option = 'webpack-dev-server ' + build + serverIp + ' --port ' + package.port + ' --mode development --config src/webpack/webpack.config.js';
+  var option = 'webpack-dev-server ' + build + serverIp + ' --port ' + port + ' --mode development --config src/webpack/webpack.config.js';
   exec(option, function (err, stdout, stderr) {
     if (err) {
       console.error(`exec error: ${err}`);
@@ -24,9 +29,27 @@ module.exports = function () {
     }
     //console.log(`stdout: ${stdout}`);
   });
-  console.log('server running at ' + url + '');
+  console.log('server running at ' + url + port + '/');
   setTimeout(function () {
-    exec('start ' + url + '');//打开浏览器窗口，设置延时打开窗口，很多时候服务还没启动，窗口就打开了，显示出错
+    exec('start ' + url + port + '/');//打开浏览器窗口，设置延时打开窗口，很多时候服务还没启动，窗口就打开了，显示出错
   }, 3000);
   watch('server');
 };
+
+// 检测端口有没在使用
+function portIsOccupied(port, url) {
+  const server = net.createServer().listen(port, url);
+  //const server=net.createServer().listen(port)
+  server.on('listening', () => {
+    // console.log(`端口可用 ${port}`);
+    server.close();
+    return port
+  });
+  server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      portIsOccupied(port + 1, url); // 不可用时加1
+      console.log(`this port ${port} is occupied.try another.`)
+    }
+  });
+  return port
+}
