bf06be592d1c1ad0cca99127eb2910063bf54d24
337547038 <337547038@qq.com>
2019-01-08 09:12:34 +0800

    修复端口检测

--- guilin/lib/server.js
+++ guilin/lib/server.js
@@ -19,37 +19,41 @@ module.exports = function () {
     url = 'http://' + package.serverIp + ':';
     portUrl = package.serverIp
   }
-  const port = portIsOccupied(package.port, portUrl);
-  //var option = 'webpack-dev-server --progress --colors --hot --inline ' + build + serverIp + ' --port ' + package.port + ' ';
-  var option = 'webpack-dev-server ' + build + serverIp + ' --port ' + port + ' --mode development --config src/webpack/webpack.config.js';
-  exec(option, function (err, stdout, stderr) {
-    if (err) {
-      console.error(`exec error: ${err}`);
-      return;
-    }
-    //console.log(`stdout: ${stdout}`);
+  const port = portIsOccupied(parseInt(package.port), portUrl, function (port) {
+
+    //var option = 'webpack-dev-server --progress --colors --hot --inline ' + build + serverIp + ' --port ' + package.port + ' ';
+    var option = 'webpack-dev-server ' + build + serverIp + ' --port ' + port + ' --mode development --config src/webpack/webpack.config.js';
+    exec(option, function (err, stdout, stderr) {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+      //console.log(`stdout: ${stdout}`);
+    });
+    console.log('server running at ' + url + port + '/');
+    setTimeout(function () {
+      exec('start ' + url + port + '/');//打开浏览器窗口，设置延时打开窗口，很多时候服务还没启动，窗口就打开了，显示出错
+    }, 3000);
+    watch('server');
+
   });
-  console.log('server running at ' + url + port + '/');
-  setTimeout(function () {
-    exec('start ' + url + port + '/');//打开浏览器窗口，设置延时打开窗口，很多时候服务还没启动，窗口就打开了，显示出错
-  }, 3000);
-  watch('server');
 };
 
 // 检测端口有没在使用
-function portIsOccupied(port, url) {
+function portIsOccupied(port, url, callback) {
   const server = net.createServer().listen(port, url);
   //const server=net.createServer().listen(port)
   server.on('listening', () => {
     // console.log(`端口可用 ${port}`);
     server.close();
-    return port
+    // return port
+    callback(port)
   });
   server.on('error', (err) => {
     if (err.code === 'EADDRINUSE') {
-      portIsOccupied(port + 1, url); // 不可用时加1
+      portIsOccupied(port + 1, url, callback); // 不可用时加1
       console.log(`this port ${port} is occupied.try another.`)
     }
   });
-  return port
+  // return port
 }
