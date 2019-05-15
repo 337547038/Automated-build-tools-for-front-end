1c93d6a7e6d9f7c263aebecf2245f5f809867206
337547038 <337547038@qq.com>
2019-05-15 16:08:45 +0800

    将phantom换为puppeteer

--- guilin/lib/phantom.js
+++ guilin/lib/phantom.js
@@ -1,45 +1,23 @@
-var driver = require('node-phantom-simple');
+const puppeteer = require('puppeteer');
 var fs = require("fs");
+module.exports = async function (filename) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-module.exports = function (filename) {
-    driver.create({path: require('phantomjs').path}, function (err, browser) {
-        return browser.createPage(function (err, page) {
-            //这里改为以本地方式打开，可以不用开服务
-            //var url = 'http://localhost:' + port + '/src/model/' + filename;
-            var url = 'file:///' + process.cwd().replace(/\\/g, '/') + "/src/model/" + filename;
-            return page.open(encodeURI(url), function (err, status) {
-                if (status == 'fail') {
-                    console.log('404 error,' + filename + ' file does not exist');
-                    //process.exit(0);
-                } else {
-                    setTimeout(function () {
-                        return page.evaluate(function () {
-                            return document.getElementById('page').innerHTML;
-                        }, function (err, result) {
-                            //console.log(result);
-                            fs.writeFile('./src/model/cache/' + encodeURIComponent(filename), result, {
-                                encoding: 'utf8'
-                            }, function () {
-                                console.log('./src/model/cache/' + filename);
-                            });
-                            browser.exit();
-                        });
-                    }, 100);
-                }
-            });
-        });
-    });
-    var dist = './src/model/cache';
-    fs.exists(dist, function (exists) {
-        // 已存在
-        if (exists) {
-        }
-        // 不存在
-        else {
-            fs.mkdir(dist, function () {
-            });
-        }
-    });
-};
+  var url = 'file:///' + process.cwd().replace(/\\/g, '/') + "/src/model/" + filename;
+  await page.goto(url);
 
+  const dimensions = await page.evaluate(() => {
+    return document.getElementById('page').innerHTML
+  });
 
+  fs.writeFile('./src/model/cache/' + encodeURIComponent(filename), dimensions, {
+    encoding: 'utf8'
+  }, function () {
+    console.log('./src/model/cache/' + filename);
+  });
+
+  // console.log('Dimensions:', dimensions);
+
+  await browser.close();
+}
