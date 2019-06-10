ef170b05f0be546087c8f512ec865a43b87933b6
337547038 <337547038@qq.com>
2019-06-10 09:56:39 +0800

    c

--- guilin.cmd
+++ guilin.cmd
@@ -1,7 +1,7 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\guilin\bin\guilin" %*
+  "%~dp0\node.exe"  "%~dp0\guilin\bin\guilin" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\guilin\bin\guilin" %*
+  node  "%~dp0\guilin\bin\guilin" %*
 )
